fcebfce0888a31d445f4e6eb26150cea5f5ffb7a
Khin Chan Myae Htun <52129905+khinchanmyaehtun@users.noreply.github.com>
2020-09-16 21:40:14 +0630

    Add files via upload

---
+++ install_user_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## Install Codedeloy agent on ec2
+
+sudo yum update
+sudo yum install ruby
+sudo yum install wget
+cd /home/ec2-user
+wget https://aws-codedeploy-ap-southeast-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent status
